6563b41898a67243acaa0127e9f1935e0a89c9b4
Ahmet Efe Gençel <bloggeromeo@gmail.com>
2026-05-08 12:35:49 +0300

    Initial commit

---
+++ src/app/login/login.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // Burada login işlemi yapılacak
+    }
+  }
+
+
+}
